ea2534b95cc1ff47ef6d329b1e9831030836eb66
prince <nyankonton100@gmail.com>
2021-06-28 01:34:18 -0300

    added search"

--- javaScript.js
+++ javaScript.js
@@ -1,6 +1,26 @@
+//search element
+function search(s){
+    
+    if (s.matches){
+        
+        document.getElementById("search-contaniner").innerHTML ="<span><input type='text' placeholder='Search here'/><button class='search-btn'><i class='fas fa-search'</button></span>"
+        document.getElementById("search-inHeader").innerHTML =""
+    }else{
+        document.getElementById("search-inHeader").innerHTML ="<span><input type='text' placeholder='Search here'/><button class='search-btn'><i class='fas fa-search'</button></span>"
+        document.getElementById("search-contaniner").innerHTML =""
+        
+    }
+}
+
+var s = window.matchMedia("(max-width: 700px)")
+search(s)
+s.addListener(search);
+console.log(s);
+
+
 // for the comfirm password
-document.querySelector('.signup-button').onclick = function(){
-    var password = document.querySelector('#password').value,
+document.querySelector('signup-button').onclick = function(){
+    const password = document.querySelector('#password').value,
         passwordconfirm = document.querySelector('#passwordconfirm').value;
     
     if(password != passwordconfirm){
@@ -11,3 +31,4 @@ document.querySelector('.signup-button').onclick = function(){
         return true
     }
 }
+
